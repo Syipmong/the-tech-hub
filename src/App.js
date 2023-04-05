8658a482e4467f42703b9f3f0f2c82e46a236fd7
syipmong <syipmong5@gmail.com>
2023-04-05 09:08:57 +0100

    debugged error

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
 import './App.css';
-import { createTheme, ThemeProvider } from '@material-ui/core';
-import CustomBtn from './components/CustomBtn';
+import { createTheme, ThemeProvider, makeStyles} from '@material-ui/core';
 
+import NavBar from './components/NavBar';
+import Grid from './components/Grid';
+import Footer from './components/Footer';
+import ComputerIcon from '@material-ui/icons/Computer'
+import SecurityIcon from '@material-ui/icons/Security'
+import EventNoteIcon from '@material-ui/icons/EventNote'
+import TrendingUpIcon from '@material-ui/icons/TrendingUp'
+import ImportExportIcon from '@material-ui/icons/ImportExport'
+import HttpIcon from '@material-ui/icons/Http'
 const theme = createTheme(
   {
     palette: {
@@ -31,11 +39,33 @@ const theme = createTheme(
 )
 
 function App() {
+  const classes = styles();
   return (
     <div className="App">
-      <ThemeProvider theme={theme}>
-    <CustomBtn txt = "Hello"/>
-        </ThemeProvider>
+            <ThemeProvider theme={theme}>
+        <NavBar/>
+        <div className={classes.wrapper}>
+          <Typography variant="h4" className={classes.bigSpace} color="primary">
+             At Rocket.io we are passionate about software
+          </Typography>
+          <Typography variant="h5" className={classes.littleSpace} color="primary">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean sodales congue tristique. Cras non pretium sem. Duis interdum lorem sit amet ligula pretium, sed rutrum urna semper. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Phasellus egestas gravida ullamcorper.
+          </Typography>
+        </div>
+        <div className={`${classes.grid} ${classes.bigSpace}`}>
+          <Grid icon={<SecurityIcon style={{fill: "#4360A6", height:"125", width:"125"}}/>}  title="Secure" btnTitle="Show me More" />
+          <Grid icon={<EventNoteIcon style={{fill: "#449A76", height:"125", width:"125"}}/>} title="Reliable" btnTitle="Show me More"/>
+          <Grid icon={<TrendingUpIcon style={{fill: "#D05B2D", height:"125", width:"125"}}/>}  title="Performant" btnTitle="Show me More"/>
+        </div>
+        <div className={`${classes.grid} ${classes.littleSpace}`}>  
+          <Grid icon={<ImportExportIcon style={{fill: "#5EA780", height:"125", width:"125"}}/>}  title="Modular" btnTitle="Show me More"/>
+          <Grid icon={<ComputerIcon style={{fill: "#E69426", height:"125", width:"125"}}/>}  title="Multi-Platform" btnTitle="Show me More"/>
+          <Grid icon={<HttpIcon style={{fill: "#2EA09D", height:"125", width:"125"}}/>} title="Connected" btnTitle="Show me More"/>
+        </div>
+        <div className={classes.bigSpace}>
+          <Footer/>
+        </div>
+      </ThemeProvider>
     </div>
   );
 }
