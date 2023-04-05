c262d3beaf65876e8c72feda030c0b9d92bb14b0
syipmong <syipmong5@gmail.com>
2023-04-05 03:20:33 +0100

    debugged succesfully

--- src/App.js
+++ src/App.js
@@ -1,9 +1,41 @@
 import './App.css';
+import { createTheme, ThemeProvider } from '@material-ui/core';
+import CustomBtn from './components/CustomBtn';
+
+const theme = createTheme(
+  {
+    palette: {
+      primary: {
+        main: "#2e1667",
+
+      },
+      secondary: {
+        main: "#c7d8ed",
+      },
+    },
+    typography: {
+      fontFamily: [
+        "roboto",
+      ],
+      h4: {
+        fontWeight: 600,
+        fontSize: 28,
+        lineHeight: "2rem",
+      },
+      h5: {
+        fontWeight: 100,
+        lineHeight: '2rem',
+      }
+    }
+  }
+)
 
 function App() {
   return (
     <div className="App">
-      <h1>THE TECH HUB SITE</h1>
+      <ThemeProvider theme={theme}>
+    <CustomBtn txt = "Hello"/>
+        </ThemeProvider>
     </div>
   );
 }
